4edae53855db643a525983fd01df897e192d3d64
Caio Cezar Dias <caiocd007@gmail.com>
2025-01-26 22:00:40 -0300

    [Feature]: added model Workout.cs

---
+++ virtusstructura-backend/Models/Workout.cs
@@ -0,0 +1,19 @@
+﻿namespace virtusstructura_backend.Models
+{
+    public class Workout
+    {
+        public int Id { get; set; }
+
+        // Informações do treino
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+
+        // Relacionamento com microciclo
+        public int MicrocycleId { get; set; }
+        public Microcycle Microcycle { get; set; }
+
+        // Relacionamento com exercícios
+        public ICollection<Exercise> Exercises { get; set; }
+    }
+
+}
