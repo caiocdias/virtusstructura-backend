20335dbbe2e7a0cd377022d782a46e2c7a52563c
Caio Cezar Dias <caiocd007@gmail.com>
2025-01-26 22:01:01 -0300

    [Feature]: added model Microcycle.cs

---
+++ virtusstructura-backend/Models/Microcycle.cs
@@ -0,0 +1,20 @@
+﻿namespace virtusstructura_backend.Models
+{
+    public class Microcycle
+    {
+        public int Id { get; set; }
+
+        // Informações do microciclo
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        // Relacionamento com mesociclo
+        public int MesocycleId { get; set; }
+        public Mesocycle Mesocycle { get; set; }
+
+        // Relacionamento com treinos
+        public ICollection<Workout> Workouts { get; set; }
+    }
+
+}
