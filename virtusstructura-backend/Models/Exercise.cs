5a8bf25aa78ff2014ff8753e35d8d58cb0aad23f
Caio Cezar Dias <caiocd007@gmail.com>
2025-01-26 22:00:28 -0300

    [Feature]: added model Exercise.cs

---
+++ virtusstructura-backend/Models/Exercise.cs
@@ -0,0 +1,18 @@
+﻿namespace virtusstructura_backend.Models
+{
+    public class Exercise
+    {
+        public int Id { get; set; }
+
+        // Informações do exercício
+        public string Name { get; set; }
+        public int Sets { get; set; }
+        public int Reps { get; set; }
+        public double Load { get; set; }
+
+        // Relacionamento com treino
+        public int WorkoutId { get; set; }
+        public Workout Workout { get; set; }
+    }
+
+}
