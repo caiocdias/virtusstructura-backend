887d8a7e960d37481df601b8159379ae25d2668b
Caio Cezar Dias <caiocd007@gmail.com>
2025-01-26 22:01:11 -0300

    [Feature]: added model Mesocycle.cs

---
+++ virtusstructura-backend/Models/Mesocycle.cs
@@ -0,0 +1,20 @@
+﻿namespace virtusstructura_backend.Models
+{
+    public class Mesocycle
+    {
+        public int Id { get; set; }
+
+        // Informações do mesociclo
+        public string Name { get; set; }
+        public string Objective { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        // Relacionamento com macrociclo
+        public int MacrocycleId { get; set; }
+        public Macrocycle Macrocycle { get; set; }
+
+        // Relacionamento com microciclos
+        public ICollection<Microcycle> Microcycles { get; set; }
+    }
+}
