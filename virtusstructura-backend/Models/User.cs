97be8de44b11026917a322e0925638bf6cbee7ed
Caio Cezar Dias <caiocd007@gmail.com>
2025-01-26 22:00:12 -0300

    [Feature]: added model User.cs

---
+++ virtusstructura-backend/Models/User.cs
@@ -0,0 +1,23 @@
+﻿namespace virtusstructura_backend.Models
+{
+    public class User
+    {
+        public int Id { get; set; } // Identificador único (PK)
+
+        // Informações básicas do usuário
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; } // Hash seguro da senha
+
+        // Dados físicos do usuário
+        public int Age { get; set; }
+        public double Weight { get; set; }
+        public double Height { get; set; }
+
+        // Nível de experiência no treinamento
+        public string ExperienceLevel { get; set; } // Ex.: "Iniciante", "Avançado"
+
+        // Relacionamento com planos de treino
+        public ICollection<TrainingPlan> TrainingPlans { get; set; }
+    }
+}
