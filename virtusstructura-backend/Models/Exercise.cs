646f198802146652ba23951fb6a0ef813feb9476
Caio Cezar Dias <caiocd007@gmail.com>
2025-02-13 17:24:22 -0300

    [Feature]: Models rework, Entity Framework integrated

--- virtusstructura-backend/Models/Exercise.cs
+++ virtusstructura-backend/Models/Exercise.cs
@@ -1,18 +1,35 @@
-﻿namespace virtusstructura_backend.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace virtusstructura_backend.Models
 {
     public class Exercise
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
-        // Informações do exercício
-        public string Name { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
         public int Sets { get; set; }
+
+        [Required]
         public int Reps { get; set; }
+
+        [Required]
         public double Load { get; set; }
 
-        // Relacionamento com treino
-        public int WorkoutId { get; set; }
-        public Workout Workout { get; set; }
-    }
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime CreatedAt { get; set; }
 
+        public DateTime UpdatedAt { get; set; }
+
+        public void SetUpdatedAt()
+        {
+            UpdatedAt = DateTime.Now;
+        }
+    }
 }
